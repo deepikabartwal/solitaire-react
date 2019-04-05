9b2b70c4918a1cfa41eb7a2f2f5fffc4bd3a3e91
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2019-04-05 09:29:10 +0530

    initial commit

---
+++ src/deck.js
@@ -0,0 +1,11 @@
+import React,{Component}from 'react';
+import ReactDOM from 'react-dom';
+
+class Deck extends Component{
+	render(){
+		return(
+			<div>deck</div>
+		)
+	}
+}
+export default Deck;
---
+++ src/generateDeck.js
@@ -0,0 +1,5 @@
+const generateCard = function(){
+	const type = ["spade","club","heart","diamond"];
+	const deck = [];
+
+}
